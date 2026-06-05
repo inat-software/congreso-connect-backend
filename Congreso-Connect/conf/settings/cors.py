8ff0ae5964d348b44f32b28ff010df70939e485d
ray <raydelochilalvarez@gmail.com>
2026-06-05 13:04:26 -0400

    implementando login

--- Congreso-Connect/conf/settings/cors.py
+++ Congreso-Connect/conf/settings/cors.py
@@ -8,7 +8,7 @@ from ._env import config
 
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS',
-    default='http://localhost:4200,http://127.0.0.1:4200,http://localhost:5173,http://127.0.0.1:5173',
+    default='http://localhost:4200,http://127.0.0.1:4200,http://localhost:5173,http://127.0.0.1:5173,http://localhost:8080,http://127.0.0.1:8080',
     cast=Csv(),
 )
 
